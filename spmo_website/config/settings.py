921af216bf6f7d091935fbdf222b8df9e465cc05
Aaron <ajbasa@up.edu.ph>
2026-01-07 15:59:39 +0800

    Configured settings for production deployment

--- spmo_website/config/settings.py
+++ spmo_website/config/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8t%l20b)o0sjbse2yu$surohb07om_*(_1q3%o@0d05*2_ue1t'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8t%l20b)o0sjbse2yu$surohb07om_*(_1q3%o@0d05*2_ue1t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
